6311631d5573698f4081c077989e416224c8b857
Ismael Navarro <ismanapa@gmail.com>
2018-06-03 23:20:36 +0200

    refactor getOptionsQueried

--- weatherService.js
+++ weatherService.js
@@ -2,17 +2,11 @@ const config = require('./config');
 const axios = require('axios');
 var emoji = require('node-emoji');
 
-const getOptionsQueried = (options) => {
-    let optionsQueried = '';
-    for(let key in options) {
-        optionsQueried += `&${key}=${options[key]}`;
-    }
-    return optionsQueried;
-}
+const getOptionsQueried = options => Object.keys(options).reduce((accumulator, key) => `&${key}=${options[key]}`, '');
 
 module.exports = {
     getCurrentWeather: (location, options) => {
-         return axios
+        return axios
             .get(`${config.API_ENDPOINT}?q=${location}&appid=${config.API_KEY}${getOptionsQueried(options)}`)
             .then(response => {
                 return new Promise((resolve, reject) => {
@@ -23,27 +17,27 @@ module.exports = {
 
     getWeatherIcon: (weatherCode) => {
         // Thunderstorm
-        if(weatherCode < 300) {
+        if (weatherCode < 300) {
             return emoji.emojify(':thunder_cloud_and_rain:');
         }
         // Drizzle
-        else if(weatherCode < 500) { 
+        else if (weatherCode < 500) {
             return emoji.emojify(':partly_sunny_rain:');
         }
         // Rain
-        else if(weatherCode < 600) {
+        else if (weatherCode < 600) {
             return emoji.emojify(':rain_cloud:');
         }
         // Snow
-        else if(weatherCode < 700) {
+        else if (weatherCode < 700) {
             return emoji.emojify(':snowman:');
         }
         // Fog
-        else if(weatherCode < 800) {
+        else if (weatherCode < 800) {
             return emoji.emojify(':fog:');
         }
         // Clear
-        else if(weatherCode == 800) {
+        else if (weatherCode == 800) {
             return emoji.emojify(':sunny:');
         }
         // Clouds
